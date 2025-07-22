d933e2811a2ec5d2c1b6d1cbd615e420981693d4
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-07-22 17:25:17 +0300

    feat: Add state management to edit selected item form

--- src/components/EditSelectedItem.tsx
+++ src/components/EditSelectedItem.tsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 
 export default function EditSelectedItem({ item }: any) {
   const [openAccordion, setOpenAccordion] = useState(false);
+  const [editedData, setEditedData] = useState({
+    title: item.title,
+    username: item.username,
+    password: item.password,
+    url: item.url,
+  });
 
   return (
     <>
@@ -9,8 +15,8 @@ export default function EditSelectedItem({ item }: any) {
         onClick={() => setOpenAccordion(!openAccordion)}
         className="cursor-pointer"
       >
-        <h2 className="mb-4">Edit</h2>
-        <div hidden={!openAccordion}>
+        <h2>Edit</h2>
+        <div hidden={!openAccordion} className="mt-4">
           <form id="selected-item-form">
             <div>
               <input
@@ -18,34 +24,43 @@ export default function EditSelectedItem({ item }: any) {
                 type="text"
                 id="title"
                 name="title"
-                value={item.title}
+                value={editedData.title}
+                onChange={(e) =>
+                  setEditedData({ ...editedData, title: e.target.value })
+                }
               />
             </div>
             <div>
               <input
                 className="w-full"
                 type="text"
-                id="title"
-                name="title"
-                value={item.username}
+                name="username"
+                value={editedData.username}
+                onChange={(e) =>
+                  setEditedData({ ...editedData, username: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <input
+                className="w-full"
+                type="password"
+                name="password"
+                value={editedData.password}
+                onChange={(e) =>
+                  setEditedData({ ...editedData, password: e.target.value })
+                }
               />
             </div>
             <div>
               <input
                 className="w-full"
                 type="text"
-                id="title"
-                name="title"
-                value={item.password}
-              />
-            </div>
-            <div>
-              <input
-                className="w-full"
-                type="text"
-                id="title"
-                name="title"
-                value={item.url}
+                name="url"
+                value={editedData.url}
+                onChange={(e) =>
+                  setEditedData({ ...editedData, url: e.target.value })
+                }
               />
             </div>
             <button className="cursor-pointer btn-hover">Edit</button>
